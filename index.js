67474d410e7fcbc8564b43f1ee5479c12b443fab
Erik Russell <erikrussells@gmail.com>
2022-09-25 12:14:15 -0400

    commit tree 23d61485398d31c662b531b01f7c6617473576ba

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     const text = document.querySelector("p");
+//     document.innerHTML = "This is really cool!"
+//     console.log(text);
+//   });
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+  })
